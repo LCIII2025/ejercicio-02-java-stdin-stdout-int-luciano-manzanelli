ed5732c7374928bc59cd843b541983e8388805ef
113826_MANZANELLI_LUCIANO <113826@tecnicatura.frc.utn.edu.ar>
2025-04-19 22:41:41 -0300

    Add exercise 2

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -12,12 +12,12 @@ public class App
     {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
         System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(b);
+        System.out.println(c);
 
     }
 }
